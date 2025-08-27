b66b088473ac2879f917b611964486f830744913
Dimitri Calixto Arruda Martins <57041642+DimitriCalixto@users.noreply.github.com>
2025-08-27 15:03:06 -0300

    [Create] FuncaoAlertSoma.js

---
+++ FuncaoAlertSoma.js
@@ -0,0 +1,8 @@
+let num1 = 0;
+let num2 = 0;
+function verificarChute(){
+    let num1 = Number(prompt('Número 1: '));
+    let num2 = Number(prompt('Número 2: '));
+    let result = num1 + num2;
+    alert('A soma dos números é: ' + result);
+}
