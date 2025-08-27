187bbec9bd2f51cf16b3bff9922ebaf8401d5816
Dimitri Calixto Arruda Martins <57041642+DimitriCalixto@users.noreply.github.com>
2025-08-27 08:54:12 -0300

    [Create] ANDeOR.js

---
+++ ANDeOR.js
@@ -0,0 +1,17 @@
+let temDieta = true;
+let temTreino = true;
+
+if (temDieta && temTreino){
+    console.log('Você É uma pessoa saudável!')
+} else{
+    console.log('Você NÂO é uma pessoa saudável!')
+}
+
+let temLeiteQuente = false;
+let temCookies = true;
+
+if (temLeiteQuente || temCookies){
+    console.log('Você está FELIZ!')
+} else{
+    console.log('Você está TRISTE!')
+}
